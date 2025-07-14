6836c78f05a1538292c226ac33559da97ff0d3fe
quangnamttp <quangnamttp@gmail.com>
2025-07-14 22:44:58 +0700

    messenger.py

--- messenger.py
+++ messenger.py
@@ -7,19 +7,14 @@ PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 
 def send_message(recipient_id, message_text):
     url = "https://graph.facebook.com/v17.0/me/messages"
-    params = {
-        "access_token": PAGE_ACCESS_TOKEN
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
+    params = {"access_token": PAGE_ACCESS_TOKEN}
+    headers = {"Content-Type": "application/json"}
     data = {
         "recipient": {"id": recipient_id},
         "message": {"text": message_text}
     }
     try:
-        res = requests.post(url, params=params, headers=headers, json=data)
-        res.raise_for_status()
-        print(f"✅ Tin nhắn đã gửi: {message_text}")
+        requests.post(url, params=params, headers=headers, json=data).raise_for_status()
+        print(f"✅ Tin đã gửi: {message_text}")
     except Exception as e:
-        print(f"⛔ Lỗi gửi tin nhắn: {e}")
+        print(f"⛔ messenger error: {e}")

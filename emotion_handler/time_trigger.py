6db16a9aba18a203dabfd42c060311dba7a58d22
quangnamttp <quangnamttp@gmail.com>
2025-07-15 21:22:13 +0700

    time_trigger.py

--- emotion_handler/time_trigger.py
+++ emotion_handler/time_trigger.py
@@ -1,1 +1,11 @@
+import datetime
+from emotion_handler.greeting_bot import send_morning_greeting
+from emotion_handler.summary_bot import send_night_summary
+
+def trigger_events():
+    now = datetime.datetime.now().strftime("%H:%M")
+    if now == "06:00":
+        send_morning_greeting()
+    elif now == "22:00":
+        send_night_summary()
 

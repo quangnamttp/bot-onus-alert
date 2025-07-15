e5e3a1a4552ce73bec5b84fd568077e65587e61b
quangnamttp <quangnamttp@gmail.com>
2025-07-15 21:03:31 +0700

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,9 @@
+BOT_STYLE = "concise"
+DEFAULT_EXCHANGE = "Binance"
+LOCAL_CURRENCY = "VND"
+TIMEZONE = "Asia/Ho_Chi_Minh"
+
+DAILY_TRIGGER_TIMES = ["06:00", "07:30", "22:00"]
+
+ALTCOIN_VOLUME_THRESHOLD = 2.5  # Volume tăng >2.5x gọi là dòng tiền vào
 

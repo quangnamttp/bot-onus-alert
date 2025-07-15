4c16d358588f7e54af70eb929008a0bc12c94408
quangnamttp <quangnamttp@gmail.com>
2025-07-15 21:19:07 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from emotion_handler.time_trigger import trigger_events
+from scheduler.cron_manager import start_cron
+
+if __name__ == "__main__":
+    print("🚀 Cofure Bot v1.2 starting...")
+    trigger_events()
+    start_cron()
 

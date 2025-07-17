d2b59ceb2e7f68a5989ebae6a15d52feed237206
quangnamttp <quangnamttp@gmail.com>
2025-07-17 22:02:39 +0700

    main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from mess_handler import send_message
 from version_handler import show_version
 
 show_version()
-send_message("📈 Bot Cofure đang hoạt động!", platform="Messenger")
+send_message("📈 Cofure Bot đã kích hoạt — chờ tín hiệu sắp tới!", platform="Messenger")

cc23158d6ac24fda3d8e078670feacbae2cb4176
quangnamttp <quangnamttp@gmail.com>
2025-07-17 22:07:39 +0700

    risk_manager.py

--- risk_manager.py
+++ risk_manager.py
@@ -1,4 +1,4 @@
-def calc_sl_tp(entry, sl_percent, tp_percent):
+def calc_sl_tp(entry, sl_percent=0.01, tp_percent=0.012):
     sl = entry * (1 - sl_percent)
     tp = entry * (1 + tp_percent)
     return round(sl, 2), round(tp, 2)

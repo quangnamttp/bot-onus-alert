f3a6d98e2f81b782f2a5492c6288b8712a7b2495
quangnamttp <quangnamttp@gmail.com>
2025-07-17 22:19:53 +0700

    auto_reporter.py

--- scheduler/auto_reporter.py
+++ scheduler/auto_reporter.py
@@ -1,23 +1,6 @@
-# auto_reporter.py
+from mess_handler import send_message
+from report.end_day_report import generate_report
 
-import json
-from datetime import datetime
-
-def log_signal(signal_text, coin, exchange):
-    log_entry = {
-        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "coin": coin,
-        "exchange": exchange,
-        "message": signal_text
-    }
-
-    try:
-        with open("signal_logs.json", "r", encoding="utf-8") as f:
-            logs = json.load(f)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        logs = []
-
-    logs.append(log_entry)
-
-    with open("signal_logs.json", "w", encoding="utf-8") as f:
-        json.dump(logs, f, ensure_ascii=False, indent=4)
+def send_summary():
+    msg = generate_report()
+    send_message(msg)

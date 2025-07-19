76229926929ee5f354becb9e91b66a2720a55c51
quangnamttp <quangnamttp@gmail.com>
2025-07-19 12:59:11 +0700

    Delete core/strategy_classifier.py

--- core/strategy_classifier.py
+++
@@ -1,6 +0,0 @@
-def classify_strategy(rsi, funding):
-    if rsi < 45 and funding < 0:
-        return "Scalping"
-    elif rsi > 50 and abs(funding) > 0.01:
-        return "Swing"
-    return "Scalping"

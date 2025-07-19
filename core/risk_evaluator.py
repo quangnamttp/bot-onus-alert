c1115e796765d2db43e9842fa17fcd6b13c3e6c8
quangnamttp <quangnamttp@gmail.com>
2025-07-19 13:02:40 +0700

    Delete core/risk_evaluator.py

--- core/risk_evaluator.py
+++
@@ -1,6 +0,0 @@
-def evaluate_risk(rsi, volume, funding):
-    score = 0
-    if rsi < 30 or rsi > 70: score += 1
-    if volume < 200000: score += 1
-    if abs(funding) > 0.03: score += 1
-    return score  # 0 = an toàn, 3 = rủi ro cao

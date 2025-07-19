3eeccd651fada371dc57e779fdcbdfb8dcebaeb8
quangnamttp <quangnamttp@gmail.com>
2025-07-19 12:34:32 +0700

    Delete scheduler/event_watcher.py

--- scheduler/event_watcher.py
+++
@@ -1,8 +0,0 @@
-from mess_handler import send_message
-
-def check_macro_event():
-    msg = (
-        "🔔 Lưu ý: hôm nay có tin CPI lúc 19h30 & FOMC lúc 22h00\n"
-        "⚡ Biến động mạnh có thể xảy ra → cân nhắc quản lý vốn!"
-    )
-    send_message(msg)

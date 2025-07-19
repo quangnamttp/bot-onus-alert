6112c74c59922b881fa76d1eb975c585120facb6
quangnamttp <quangnamttp@gmail.com>
2025-07-19 12:44:21 +0700

    cache_manager.py

---
+++ core/cache_manager.py
@@ -0,0 +1,24 @@
+import json
+
+CACHE_PATH = "data/used_coins.json"
+
+def get_used_today():
+    try:
+        with open(CACHE_PATH, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def mark_used(coin):
+    try:
+        used = get_used_today()
+        if coin not in used:
+            used.append(coin)
+            with open(CACHE_PATH, "w") as f:
+                json.dump(used, f, indent=2)
+    except:
+        pass
+
+def reset_coin_state():
+    with open(CACHE_PATH, "w") as f:
+        json.dump([], f)

b1b4d5e084bb7f3c3a3dac68061ec83dab209ac1
quangnamttp <quangnamttp@gmail.com>
2025-07-19 08:02:08 +0700

    Delete utils/validator.py

--- utils/validator.py
+++
@@ -1,9 +0,0 @@
-def validate_signal(signal):
-    required_keys = ["symbol", "entry", "sl", "tp", "strategy", "rr"]
-    return all(k in signal and signal[k] is not None for k in required_keys)
-
-def validate_price(price):
-    return isinstance(price, (int, float)) and price > 0
-
-def validate_percent(value):
-    return isinstance(value, float) and 0 <= value <= 1

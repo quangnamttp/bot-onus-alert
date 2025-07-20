ab2283e23f5223ea5b9be5bbee6d04d93e7f41e4
quangnamttp <quangnamttp@gmail.com>
2025-07-20 20:40:50 +0700

    Delete marketdata directory

--- marketdata/liquidity_detector.py
+++
@@ -1,6 +0,0 @@
-def detect_liquidity_zones(price, recent_levels):
-    zones = []
-    for level in recent_levels:
-        if abs(price - level) / price < 0.01:
-            zones.append(level)
-    return zones
--- marketdata/volume_tracker.py
+++
@@ -1,7 +0,0 @@
-def track_volume(data):
-    results = {}
-    for coin in data:
-        symbol = coin["symbol"]
-        volume = coin["volume"]
-        results[symbol] = volume
-    return results

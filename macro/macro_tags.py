5579fe87bce7f52a32795f2e22e56c69ae9e9a04
quangnamttp <quangnamttp@gmail.com>
2025-07-22 20:37:03 +0700

    Delete macro directory

--- macro/macro_tags.py
+++
@@ -1,7 +0,0 @@
-def tag_event(event_text):
-    if "CPI" in event_text or "FOMC" in event_text:
-        return "🔴 Cao"
-    elif "NFP" in event_text or "Lãi suất" in event_text:
-        return "🟠 Trung bình"
-    else:
-        return "🟢 Thấp"

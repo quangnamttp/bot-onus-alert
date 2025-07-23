e7baef56d593f93876eea42c9f72935e7a188335
quangnamttp <quangnamttp@gmail.com>
2025-07-23 20:48:07 +0700

    Delete utils/logger.py

--- utils/logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def log(content):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("data/logs.txt", "a") as f:
-        f.write(f"[{timestamp}] {content}\n")

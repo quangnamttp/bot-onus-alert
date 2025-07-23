a240ed6c0ba029c4cdeb495ea04260c07320e3e4
quangnamttp <quangnamttp@gmail.com>
2025-07-23 11:19:57 +0700

    Update send_message.py

--- messenger/send_message.py
+++ messenger/send_message.py
@@ -10,6 +10,6 @@ def send_message(recipient_id, message_text):
     headers = {
         "Content-Type": "application/json"
     }
-    url = f"https://graph.facebook.com/v18.0/me/messages?access_token={EAAUfsOzztLEBPKQKkm5BuLuBXMXzUfsEb6UZAfTZBRi8YZCkog3GlrcZB3EgGVfS3pGeO7s0s1x0nsWQBkyZCACE3fyl7dluU2rFu6raEk7rCPzDFQBHCwZChttww36WIKfQ6Ua3ZBpyNfCadOkG8AzCKfiIcbLAei8P7ql1b2eAnKITJohDpauAh0l0ZAZCesfusIJhyJRwveQZDZD}"
+    url = f"https://graph.facebook.com/v18.0/me/messages?access_token={PAGE_ACCESS_TOKEN}"
     response = requests.post(url, json=payload, headers=headers)
     print(f"[send_message] → {recipient_id}: {message_text} | Status: {response.status_code}")

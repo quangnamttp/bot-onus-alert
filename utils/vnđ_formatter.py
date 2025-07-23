502481fddc3b78d15285e81bb8d7ed6790199103
quangnamttp <quangnamttp@gmail.com>
2025-07-23 18:34:58 +0700

    Update vnđ_formatter.py

--- utils/vnđ_formatter.py
+++ utils/vnđ_formatter.py
@@ -1,2 +1,6 @@
-def format_vnd(amount):
-    return f"{amount:,.0f} VNĐ"
+def format(vnd_amount):
+    try:
+        amount = int(str(vnd_amount).replace(",", ""))
+        return "{:,} VNĐ".format(amount)
+    except:
+        return f"{vnd_amount} VNĐ"

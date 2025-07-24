2e72b4b6243ec8359b491cb1956b72adf5d57b1f
quangnamttp <quangnamttp@gmail.com>
2025-07-24 10:16:55 +0700

    Update news_schedule.py

--- scheduler/news_schedule.py
+++ scheduler/news_schedule.py
@@ -1,11 +1,17 @@
-# cofure_bot/scheduler/news_schedule.py
-
 from macro.forex_factory_fetcher import fetch_macro_news
 from macro.macro_advisor import generate_macro_strategy
 from messenger.send_message import send_message
 from utils.config_loader import MY_USER_ID
 
-def send_macro_news(user_id):
-    news = fetch_macro_news()
+def send_macro_news(user_id, date="today", date_range=None):
+    if date_range == "week":
+        news = fetch_macro_news(date_range="week")
+    elif date == "tomorrow":
+        news = fetch_macro_news(date="tomorrow")
+    elif date == "today":
+        news = fetch_macro_news(date="today")
+    else:
+        news = []
+
     message = generate_macro_strategy(news)
     send_message(user_id, message)

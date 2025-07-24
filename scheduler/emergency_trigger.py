6d35e33fba13ab25bc92fc9cb6239f62057e476d
quangnamttp <quangnamttp@gmail.com>
2025-07-24 12:04:39 +0700

    Update emergency_trigger.py

--- scheduler/emergency_trigger.py
+++ scheduler/emergency_trigger.py
@@ -2,11 +2,12 @@
 
 import threading, time
 from core.emergency_signal import run_emergency_monitor
+from utils.signal_switch import is_signal_enabled  # ← Thêm import công tắc
 
 def run_emergency_loop():
     def loop():
         while True:
-            run_emergency_monitor()
-            time.sleep(60)  # Quét mỗi phút
-
+            if is_signal_enabled():
+                run_emergency_monitor()
+            time.sleep(60)  # Chạy mỗi phút nếu công tắc đang ON
     threading.Thread(target=loop, daemon=True).start()
